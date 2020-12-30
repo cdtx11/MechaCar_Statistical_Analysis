83967ba0ef2d8c0ccde81b41af1fbd73e4c35ae3
cdtx11 <claire.davis.tx@gmail.com>
2020-12-30 13:47:38 -0600

    deliverable 1 r script

---
+++ MechaCarChallenge.R
@@ -0,0 +1,5 @@
+library(dplyr)
+
+MechaCar_data <- read.csv('MechaCar_mpg.csv',check.names = F,stringsAsFactors = F)
+
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_data))
